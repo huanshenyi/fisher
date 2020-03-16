b5c23f32b309e9de55000fe6dd6d47c6cb547c46
huanshenyi <txy1226052@gmail.com>
2020-03-16 23:57:02 +0900

    main関数の変更中

---
+++ app/web/book.py
@@ -0,0 +1,17 @@
+from util import is_isbn_or_key
+from yushu_book import YuShuBook
+
+
+@app.route("/book/search/<q>/<page>")
+def search(q, page):
+    """
+        q: isbn && 普通のキーワード
+        page(start, count)
+    :return:
+    """
+    isbn_or_key = is_isbn_or_key(q)
+    if isbn_or_key == 'isbn':
+        result = YuShuBook.search_by_isbn(q)
+    else:
+        result = YuShuBook.search_by_keyword(q)
+    return result
--- main.py
+++ main.py
@@ -1,29 +1,8 @@
 __author__ = "ハリネズミ"
-
-import json
-
-from flask import Flask, jsonify
-from util import is_isbn_or_key
-
-from yushu_book import YuShuBook
+from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object("config")
-
-
-@app.route("/book/search/<q>/<page>")
-def search(q, page):
-    """
-        q: isbn && 普通のキーワード
-        page(start, count)
-    :return:
-    """
-    isbn_or_key = is_isbn_or_key(q)
-    if isbn_or_key == 'isbn':
-        result = YuShuBook.search_by_isbn(q)
-    else:
-        result = YuShuBook.search_by_keyword(q)
-    return result
 
 
 if __name__ == "__main__":
